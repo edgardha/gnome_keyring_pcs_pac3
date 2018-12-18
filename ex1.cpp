c083a4b4ec073f00424633153373e5d42971c058
Edgard de Haro <edgardha@gmail.com>
2018-12-18 23:07:55 +0100

    Added some comments and changed if that check the result of find password

--- ex1.cpp
+++ ex1.cpp
@@ -10,6 +10,8 @@ void createPassword() {
     gchar password[128];
     gchar attr_name[128],attr_value[128];
     const gchar *c_display_name,*c_password,*c_attr_name,*c_attr_value;
+
+    //Read all parameters from stdin and assign it to a const gchar pointer
     printf("Enter description text: ");
     scanf("%s",display_name);
     c_display_name = display_name;
@@ -23,10 +25,20 @@ void createPassword() {
     scanf("%s", attr_value);
     c_attr_value = attr_value;
     
+    //Create a schema, defining the keyring item type and the attributes that we want to add
+    //(in this case there are only one attribute, but the list of items must be finished with a NULL item)
     const GnomeKeyringPasswordSchema schema = {GNOME_KEYRING_ITEM_GENERIC_SECRET,
     {{c_attr_name, GNOME_KEYRING_ATTRIBUTE_TYPE_STRING}, {NULL, GnomeKeyringAttributeType::GNOME_KEYRING_ATTRIBUTE_TYPE_STRING}}};
 
+    //Add the password with the attributes to the keyring and save the result
+    //(there is on argument with its value, and the last argument is NULL by function definition)
     res = gnome_keyring_store_password_sync(&schema, NULL, c_display_name, c_password, c_attr_name, c_attr_value,NULL,(char *)NULL);
+
+    if (res == GNOME_KEYRING_RESULT_OK) {
+        printf("Password added correctly.\n");
+    } else {
+        printf("There is and error to add your password.\n");
+    }
 }
 
 void getPassword() {
@@ -36,6 +48,7 @@ void getPassword() {
     gchar attr_value[128];
     const gchar *c_attr_name,*c_attr_value;
 
+    //Read all parameters from stdin and assign it to a const gchar pointer
     printf("Enter attribute name: ");
     scanf("%s", attr_name);
     c_attr_name = attr_name;
@@ -43,17 +56,24 @@ void getPassword() {
     scanf("%s", attr_value);
     c_attr_value = attr_value;
 
+    //Create a schema, defining the keyring item type and the attributes that we want to add
+    //(in this case there are only one attribute, but the list of items must be finished with a NULL item)
     const GnomeKeyringPasswordSchema schema = {GNOME_KEYRING_ITEM_GENERIC_SECRET,
     {{c_attr_name, GNOME_KEYRING_ATTRIBUTE_TYPE_STRING}, {NULL, GnomeKeyringAttributeType::GNOME_KEYRING_ATTRIBUTE_TYPE_STRING}}};
 
+    //Get the password with passed arguments and put it on pass pointer
+    //(there is on argument with its value, and the last argument is NULL by function definition)
     res = gnome_keyring_find_password_sync(&schema, &pass, c_attr_name, c_attr_value, NULL, (char *)NULL);
 
-    if (pass == NULL)
+    if (res != GNOME_KEYRING_RESULT_OK)
     {
         printf("There are no password with this attributes.\n");
         return;
     }
+
     printf("Password is %s\n", pass);
+
+    //When we don't need any more the pass, we must free it with this function
     gnome_keyring_free_password(pass);
 
     return;
